cd25aceb090daece53536ba03b0cf703facadac1
alecortiz03 <alecortiz03@gmail.com>
2024-11-28 14:36:32 -0700

    Alec playing arounud

--- bike_app/src/main/java/app/bike/Main.java
+++ bike_app/src/main/java/app/bike/Main.java
@@ -2,8 +2,9 @@ package app.bike;
 
 public class Main {
     public static void main(String[] args) {
-        String bikeRackURL = "https://data.edmonton.ca/resource/rter-xgyu.csv";
-        BikeURLClass bikeRackLocations = new BikeURLClass(bikeRackURL);
+        String bikeRackURL = "https://data.edmonton.ca/resource/vd4b-a4iv.json";
+        BikeURLClassJson bikeJson = new BikeURLClassJson(bikeRackURL);
+        System.out.println(bikeJson.URLParse());
     }
 }
 
