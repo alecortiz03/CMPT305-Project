7066bffbfbcc589ebbdaab8098721beca449ffe4
alecortiz03 <alecortiz03@gmail.com>
2024-11-28 13:02:48 -0700

    Alec - Playing with some stuff

--- bike_app/src/main/java/app/bike/Main.java
+++ bike_app/src/main/java/app/bike/Main.java
@@ -1,13 +1,8 @@
 package app.bike;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
 
 public class Main {
     public static void main(String[] args) {
-        String bikeRackURL = "https://data.edmonton.ca/resource/rter-xgyu.json";
+        String bikeRackURL = "https://data.edmonton.ca/resource/vd4b-a4iv.json";
         URLClass bikeRackLocations = new URLClass(bikeRackURL);
         System.out.println(bikeRackLocations.URLParse());
     }
