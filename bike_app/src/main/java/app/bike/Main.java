47d6c1bb4d21128ed8bf49a7075b3f81f4aff66a
Alec Ortiz <84681832+alecortiz03@users.noreply.github.com>
2024-11-28 14:38:22 -0700

    json change

--- bike_app/src/main/java/app/bike/Main.java
+++ bike_app/src/main/java/app/bike/Main.java
@@ -2,7 +2,7 @@ package app.bike;
 
 public class Main {
     public static void main(String[] args) {
-        String bikeRackURL = "https://data.edmonton.ca/resource/rter-xgyu.csv";
+        String bikeRackURL = "https://data.edmonton.ca/resource/rter-xgyu.json";
         BikeURLClass bikeRackLocations = new BikeURLClass(bikeRackURL);
     }
 }
