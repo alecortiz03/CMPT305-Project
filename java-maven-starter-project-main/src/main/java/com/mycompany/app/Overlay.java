7e857a09046774340016eb5fd96402a7a2a9ed34
Arikka <arikka.cherniwchan@gmail.com>
2024-11-29 10:50:45 -0700

    Working hopefully with Overlay class added

--- java-maven-starter-project-main/src/main/java/com/mycompany/app/Overlay.java
+++ java-maven-starter-project-main/src/main/java/com/mycompany/app/Overlay.java
@@ -8,18 +8,20 @@ import java.net.http.HttpResponse;
 
 public class Overlay {
 
-    public static void main(String[] args)throws IOException, InterruptedException {
-        String  url = "https://data.edmonton.ca/resource/65fr-66s6.json?geometry_multipolygon";
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String url = "https://data.edmonton.ca/resource/65fr-66s6.json?geometry_multipolygon";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .GET()
                 .build();
 
-        try{
+        try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             System.out.println(response.body());
-        }catch(IOException | InterruptedException e){}
+        } catch (IOException | InterruptedException e) {
+        }
 
     }
+}
 
