4079dab15cfc0af2b19dc33795654abb0277fb7b
Arikka <arikka.cherniwchan@gmail.com>
2024-12-02 10:20:57 -0700

    Working overlay on map! Added Bike routes layer!

--- java-maven-starter-project-main/src/main/java/com/mycompany/app/App.java
+++ java-maven-starter-project-main/src/main/java/com/mycompany/app/App.java
@@ -55,7 +55,7 @@ public class App extends Application {
         // An API key is required to enable access to services, web maps, and web scenes hosted in ArcGIS Online.
         // If you haven't already, go to your developer dashboard to get your API key.
         // Please refer to https://developers.arcgis.com/java/get-started/ for more information
-        String yourApiKey = "AAPTxy8BH1VEsoebNVZXo8HurKi7aeFYVUcn_aZ7X7LtCGm-0_3kgitapKiaBZcHKNzSc6JL7ygonCJarQ9k_h9iChOyrYBYJt3CfVtq6arA_yOpj50CV_lPCVK3CIIPg2tavV0bQmPfjiX-id7z8u0iKGe_eNhjOJ1tecH205U27oe90Vv9ZuIAdEjHgefWoJrjb6uK5HbqusyG-7bjQCIJkIMcDGNmE7yIQctLn_AZHgffg2wqf0jF2u20vMRi7--KAT1_MCnmWKIq";
+        String yourApiKey = "AAPTxy8BH1VEsoebNVZXo8HurKi7aeFYVUcn_aZ7X7LtCGmI5rfxHaaL21jdPPXYpp9huX2_m2G4-XJYuUh1M54mnYNwiLHC0BMYosLxdaQpXPpo3_zr-XEV2ZhUBGyR3SLTbpUKRV4Xr_hE2MQ1RlY8eArscCn0SFWrG8kKPAestlAjUkUxYpfe5cTLZ9IHTrlOiAQyfUS0Gkopiv1V_R-YZ6TT7qVerIEbbRin1LBS_06j9qtuFb37OR-_gjn0AV_tAT1_MCnmWKIq";
         ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
 
         // create a MapView to display the map and add it to the stack pane
