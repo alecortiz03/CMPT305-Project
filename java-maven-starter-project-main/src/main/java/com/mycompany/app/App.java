afc7cba8358e3d0c8fc622e891efdb1a688c90e5
Alec Ortiz <84681832+alecortiz03@users.noreply.github.com>
2024-12-05 13:57:35 -0700

    Update App.java

--- java-maven-starter-project-main/src/main/java/com/mycompany/app/App.java
+++ java-maven-starter-project-main/src/main/java/com/mycompany/app/App.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Fix
+
  package com.mycompany.app;
 
  import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
